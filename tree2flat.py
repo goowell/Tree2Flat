546a720caba2356177207fcb9e547a22578904c1
Shanwei Zhang <Shanwei Zhang>
2017-01-16 07:33:59 +0800

    refactor: rename

--- tree2flat.py
+++ tree2flat.py
@@ -7,19 +7,19 @@ def _get_value_from_dict(data, keys):
             break
     return v
 
-def _list_mapping(source, mapping):
+def _list_mapping(tree_list, mapping):
     flat_list = []
-    for l in source:
-        flat_list.append(dict_mapping(l, mapping))
+    for l in tree_list:
+        flat_list.append(tree_to_flat(l, mapping))
     return flat_list
 
-def dict_mapping(source, mapping):
+def tree_to_flat(tree_dict, mapping):
     flat_dict = {}
     for m in mapping:
         if len(m) == 2:            
-            v = _get_value_from_dict(source,m[0])
+            v = _get_value_from_dict(tree_dict,m[0])
         elif len(m) == 3:
-            v = _list_mapping(_get_value_from_dict(source,m[0]), m[2])
+            v = _list_mapping(_get_value_from_dict(tree_dict,m[0]), m[2])
         else:
             v = None
             print('invalid mapping rule: %' % m)
@@ -29,10 +29,12 @@ def dict_mapping(source, mapping):
     return flat_dict
 
 def main():
-    print(dict_mapping(in_data, mapping))
+    print(tree_to_flat(in_data, mapping))
 
 
 if __name__ == '__main__':
     import sys
     from data import mapping, in_data
+
     sys.exit(int(main() or 0))
+    
